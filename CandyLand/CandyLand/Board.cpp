0d72bb12caf5162b342bd049a9fa07aa51675a9a
Jonathan Muniz <33111715+Jo-Mu@users.noreply.github.com>
2020-04-06 21:51:44 -0400

    Added Space subclass to Board class and added functions for adding Spaces to Board, accessing Space info in Board, and getting the index of a color from a Card

--- CandyLand/CandyLand/Board.cpp
+++ CandyLand/CandyLand/Board.cpp
@@ -1,1 +1,106 @@
 #include "Board.h"
+
+Color::TileColor Board::Space::GetColor() const
+{
+	return color;
+}
+
+bool Board::Space::IsLoseTurn() const
+{
+	return loseTurn;
+}
+
+bool Board::Space::IsShortcut() const
+{
+	return isShortcut;
+}
+
+int Board::Space::GetShortcutIndex() const
+{
+	return shortcutIndex;
+}
+
+void Board::Space::SetShortcutIndex(int index)
+{
+	isShortcut = true;
+	shortcutIndex = index;
+}
+
+void Board::AddSpace(Color::TileColor color)
+{
+	brd.emplace_back(Space(color));
+}
+
+void Board::AddLoseTurnSpace(Color::TileColor color)
+{
+	brd.emplace_back(Space(color, true));
+}
+
+bool Board::IsShortcutAt(int index) const
+{
+	if (index >= 0 && index < brd.size())
+	{
+		return brd[index].IsShortcut();
+	}
+
+	return false;
+}
+
+bool Board::IsTurnLostAt(int index) const
+{
+	if (index >= 0 && index < brd.size())
+	{
+		return brd[index].IsLoseTurn();
+	}
+
+	return false;
+}
+
+Color::TileColor Board::GetColorAt(int index) const
+{
+	if (index >= 0 && index < brd.size())
+	{
+		return brd[index].GetColor();
+	}
+
+	return Color::TileColor::Empty;
+}
+
+int Board::GetNextColorSpaceIndex(int currIndex, const Card& card) const
+{
+	Color::TileColor cardColor = card.GetColor();
+
+	if (cardColor == Color::TileColor::Mint || cardColor == Color::TileColor::Lollypop
+		|| cardColor == Color::TileColor::IceCream || cardColor == Color::TileColor::Peanut) 
+	{
+		for (int index = 0; index < brd.size(); index++) 
+		{
+			if (cardColor == GetColorAt(index)) 
+			{
+				return index;
+			}
+		}
+
+		return (brd.size() - 1);
+	}
+	else 
+	{
+		int nextIndex = currIndex + 1;
+		int quantity = card.GetQuantity();
+
+		while (nextIndex < (brd.size() - 1) && quantity > 0) 
+		{
+			if (cardColor == GetColorAt(nextIndex))
+			{
+				quantity--;
+			}
+
+			if (quantity > 0) 
+			{
+				nextIndex++;
+			}
+		}
+
+		return nextIndex;
+	}
+}
--- CandyLand/CandyLand/Board.h
+++ CandyLand/CandyLand/Board.h
@@ -1,7 +1,46 @@
 #pragma once
 #include "Color.h"
+#include "Card.h"
+#include <vector>
 
 class Board
 {
+	private:
+		class Space
+		{
+			public:
+				Space(Color::TileColor spaceColor) 
+				{
+					color = spaceColor;
+					loseTurn = false;
+				}
+
+				Space(Color::TileColor spaceColor, bool isLoseTurn)
+				{
+					color = spaceColor;
+					loseTurn = isLoseTurn;
+				}
+
+				Color::TileColor GetColor() const;
+				bool IsLoseTurn() const;
+				bool IsShortcut() const;
+				int GetShortcutIndex() const;
+				void SetShortcutIndex(int index);
+
+			private:
+				Color::TileColor color;
+				bool loseTurn = false;
+				bool isShortcut = false;
+				int shortcutIndex = -1;
+		};
+	public:
+		void AddSpace(Color::TileColor color);
+		void AddLoseTurnSpace(Color::TileColor color);
+		bool IsShortcutAt(int index) const;
+		bool IsTurnLostAt(int index) const;
+		Color::TileColor GetColorAt(int index) const;
+		int GetNextColorSpaceIndex(int currIndex, const Card& card) const;
+	private:
+		std::vector<Space> brd;
 };
 
