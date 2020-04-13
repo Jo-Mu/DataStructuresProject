66fa16e6646b5e1039b8bd820becae6d17788121
Jonathan Muniz <33111715+Jo-Mu@users.noreply.github.com>
2020-04-13 12:17:06 -0400

    Fixed bugs in Board class and added comments

--- CandyLand/CandyLand/Board.cpp
+++ CandyLand/CandyLand/Board.cpp
@@ -1,53 +1,102 @@
+//File contains declarations for the methods in "Board.h"
 #include "Board.h"
 
+/*
+	Gets the color of the Space
+
+	returns a TileColor
+*/
 Color::TileColor Board::Space::GetColor() const
 {
 	return color;
 }
 
+/*
+	Checks if Space is a lose turn space
+
+	returns boolean loseTurn
+*/
 bool Board::Space::IsLoseTurn() const
 {
 	return loseTurn;
 }
 
+/*
+	Checks if Space is linked to a shortcut
+
+	returns boolean isShortcut
+*/
 bool Board::Space::IsShortcut() const
 {
 	return isShortcut;
 }
 
+/*
+	Gets the index of the shorcut the Space is linked to
+
+	returns int
+*/
 int Board::Space::GetShortcutIndex() const
 {
 	return shortcutIndex;
 }
 
+/*
+	Sets the shortcutIndex of the Space to a given index
+
+	Sets isShortcut to true
+*/
 void Board::Space::SetShortcutIndex(int index)
 {
 	isShortcut = true;
 	shortcutIndex = index;
 }
 
+/*
+	Creates a new Space in the board with the given TileColor
+
+	no return
+*/
 void Board::AddSpace(Color::TileColor color)
 {
 	brd.emplace_back(Space(color));
 }
 
+/*
+	Creates a new lose turn Space in the board with the given TileColor
+
+	no return
+*/
 void Board::AddLoseTurnSpace(Color::TileColor color)
 {
 	brd.emplace_back(Space(color, true));
 }
 
+/*
+	Links to Spaces at the given indicies as a shorcut
+
+	if the either of the indicies are outside the board
+	then nothing happens'
+
+	no return
+*/
 void Board::LinkSpaces(int index1, int index2)
 {
-	if (index1 >= 0 && index1 < brd.size() && index2 >= 0 && index2 < brd.size())
+	if (index1 >= 0 && index1 < GetTotalSpaces() && index2 >= 0 && index2 < GetTotalSpaces())
 	{
 		brd[index1].SetShortcutIndex(index2);
 		brd[index2].SetShortcutIndex(index1);
 	}
 }
 
+/*
+	Checks if there is a shortcut Space at the given index
+
+	returns a boolean
+*/
 bool Board::IsShortcutAt(int index) const
 {
-	if (index >= 0 && index < brd.size())
+	if (index >= 0 && index < GetTotalSpaces())
 	{
 		return brd[index].IsShortcut();
 	}
@@ -55,9 +104,14 @@ bool Board::IsShortcutAt(int index) const
 	return false;
 }
 
+/*
+	Checks if there is a lose turn Space at the given index
+
+	returns a boolean
+*/
 bool Board::IsTurnLostAt(int index) const
 {
-	if (index >= 0 && index < brd.size())
+	if (index >= 0 && index < GetTotalSpaces())
 	{
 		return brd[index].IsLoseTurn();
 	}
@@ -65,9 +119,16 @@ bool Board::IsTurnLostAt(int index) const
 	return false;
 }
 
+/*
+	Gets the TileColor of a Space at the given index
+
+	if outside the the board than an Empty TileColor is returned
+
+	returns a TileColor
+*/
 Color::TileColor Board::GetColorAt(int index) const
 {
-	if (index >= 0 && index < brd.size())
+	if (index >= 0 && index < GetTotalSpaces())
 	{
 		return brd[index].GetColor();
 	}
@@ -75,9 +136,16 @@ Color::TileColor Board::GetColorAt(int index) const
 	return Color::TileColor::Empty;
 }
 
+/*
+	Gets the index of the other end of a shortcut at given index
+
+	If there is no shortcut then the original index is returned
+
+	returns an int
+*/
 int Board::GetShortcutAt(int index) const
 {
-	if (index >= 0 && index < brd.size())
+	if (index >= 0 && index < GetTotalSpaces())
 	{
 		if (brd[index].IsShortcut()) 
 		{
@@ -88,16 +156,36 @@ int Board::GetShortcutAt(int index) const
 	return index;
 }
 
+/*
+	Gets the total Spaces in the board
+
+	returns an int
+*/
 int Board::GetTotalSpaces() const
 {
 	return brd.size();
 }
 
+/*
+	Gets the Index of the last index of the board
+
+	If the board is empty then -1 is retuned
+
+	returns an int
+*/
 int Board::GetLastIndex() const
 {
 	return (GetTotalSpaces() - 1);
 }
 
+/*
+	Gets the index of the next space using a given Card and the current
+	index of the player
+
+	Takes into account shortcuts
+
+	returns an int
+*/
 int Board::GetNextColorSpaceIndex(int currIndex, const Card& card) const
 {
 	Color::TileColor cardColor = card.GetColor();
@@ -105,7 +193,7 @@ int Board::GetNextColorSpaceIndex(int currIndex, const Card& card) const
 	if (cardColor == Color::TileColor::Mint || cardColor == Color::TileColor::Lollypop
 		|| cardColor == Color::TileColor::IceCream || cardColor == Color::TileColor::Peanut) 
 	{
-		for (int index = 0; index < brd.size(); index++) 
+		for (int index = 0; index < GetTotalSpaces(); index++) 
 		{
 			if (cardColor == GetColorAt(index)) 
 			{
--- CandyLand/CandyLand/Board.h
+++ CandyLand/CandyLand/Board.h
@@ -1,3 +1,7 @@
+/*
+	File contains methods, varables, and subclasses to represent
+	a Candy Land Board that can be traversed through
+*/
 #pragma once
 #include "Color.h"
 #include "Card.h"
@@ -6,45 +10,144 @@
 class Board
 {
 	private:
+		//Subclass for a Space in the Board
 		class Space
 		{
 			public:
+				/*
+					Constructor for Space
+
+					Takes a TileColor, will set loseTurn to false by default
+				*/
 				Space(Color::TileColor spaceColor) 
 				{
 					color = spaceColor;
 					loseTurn = false;
 				}
 
+				/*
+					Constructor for Space
+
+					Takes a TileColor and a boolean for whether or not 
+					it's a lose turn space
+				*/
 				Space(Color::TileColor spaceColor, bool isLoseTurn)
 				{
 					color = spaceColor;
 					loseTurn = isLoseTurn;
 				}
 
+				/*
+					Gets the color of the Space
+
+					returns a TileColor
+				*/
 				Color::TileColor GetColor() const;
+				/*
+					Checks if Space is a lose turn space
+
+					returns boolean loseTurn
+				*/
 				bool IsLoseTurn() const;
+				/*
+					Checks if Space is linked to a shortcut
+
+					returns boolean isShortcut
+				*/
 				bool IsShortcut() const;
+				/*
+					Gets the index of the shorcut the Space is linked to
+
+					returns int
+				*/
 				int GetShortcutIndex() const;
+				/*
+					Sets the shortcutIndex of the Space to a given index
+
+					Sets isShortcut to true
+				*/
 				void SetShortcutIndex(int index);
 
 			private:
-				Color::TileColor color;
-				bool loseTurn = false;
-				bool isShortcut = false;
-				int shortcutIndex = -1;
+				Color::TileColor color; //TileColor of the space
+				bool loseTurn = false; //Is Space a lose turn space
+				bool isShortcut = false; //Is Space a shortcut
+				int shortcutIndex = -1; //Index of the shortcut, -1 if none
 		};
 	public:
+		/*
+			Creates a new Space in the board with the given TileColor
+
+			no return
+		*/
 		void AddSpace(Color::TileColor color);
+		/*
+			Creates a new lose turn Space in the board with the given TileColor
+
+			no return
+		*/
 		void AddLoseTurnSpace(Color::TileColor color);
+		/*
+			Links to Spaces at the given indicies as a shorcut
+
+			if the either of the indicies are outside the board
+			then nothing happens'
+
+			no return
+		*/
 		void LinkSpaces(int index1, int index2);
+		/*
+			Checks if there is a shortcut Space at the given index
+
+			returns a boolean
+		*/
 		bool IsShortcutAt(int index) const;
+		/*
+			Checks if there is a lose turn Space at the given index
+
+			returns a boolean
+		*/
 		bool IsTurnLostAt(int index) const;
+		/*
+			Gets the TileColor of a Space at the given index
+
+			if outside the the board than an Empty TileColor is returned
+
+			returns a TileColor
+		*/
 		Color::TileColor GetColorAt(int index) const;
+		/*
+			Gets the index of the other end of a shortcut at given index
+
+			If there is no shortcut then the original index is returned
+
+			returns an int
+		*/
 		int GetShortcutAt(int index) const;
+		/*
+			Gets the total Spaces in the board
+
+			returns an int
+		*/
 		int GetTotalSpaces() const;
+		/*
+			Gets the Index of the last index of the board
+
+			If the board is empty then -1 is retuned
+
+			returns an int
+		*/
 		int GetLastIndex() const;
+		/*
+			Gets the index of the next space using a given Card and the current
+			index of the player
+
+			Takes into account shortcuts
+
+			returns an int
+		*/
 		int GetNextColorSpaceIndex(int currIndex, const Card& card) const;
 	private:
-		std::vector<Space> brd;
+		std::vector<Space> brd; //The board as a colllection of Space objects
 };
 
